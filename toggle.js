037c763ee06ed6ddc3a6afb3662b86129bb1ef37
sylar-codex <vicaremy@gmail.com>
2024-03-01 02:07:01 +0100

    added mobile nav

--- toggle.js
+++ toggle.js
@@ -1,6 +1,10 @@
 const toggle = () => {
   document.getElementById("nav-div").classList.toggle("show");
-  console.log("I am being clicked");
 };
 
 document.getElementById("menu_icon").addEventListener("click", toggle);
+
+const removeCLass = () => {
+  document.getElementById("nav-div").classList.remove("show");
+};
+document.getElementById("cancel").addEventListener("click", removeCLass);
